1b22a85dda83061987955515196f97b36cf8cb5b
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-07-07 17:01:15 +0200

    Add state transitions

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -3,6 +3,10 @@
     <template v-if="state === 'pending'">
       <img src="/party-hat.png" alt="Party Hat" class="w-32 h-32">
       <h1 class="text-4xl">Magnus 50</h1>
+    </template>
+    <template v-else-if="state === 'name'">
+      <img src="/party-hat.png" alt="Party Hat" class="w-32 h-32">
+      <h1 class="text-4xl">Magnus 50</h1>
       <h3 class="text-xl">Ange ditt namn</h3>
       <div class="flex items-center">
         <input 
@@ -23,14 +27,13 @@
       <p v-if="isNameLocked" class=" text-black text-lg">Snart drar vi igång!</p>
     </template>
     <template v-else>
-      <h1 class="text-4xl">Magnus 50</h1>
-      <h2 class="text-2xl">Välj ett namn</h2>
-      <button class="bg-blue-500 text-white p-2 rounded mt-4">Starta spelet</button>
+      <Questions />
     </template>
   </div>
 </template>
 
 <script setup>
+import Questions from '@/components/Questions.vue'
 import { ref } from 'vue'
 
 const state = ref('pending')
@@ -41,12 +44,25 @@ const toggleNameLock = () => {
   isNameLocked.value = !isNameLocked.value
 }
 
-// Allow right and left arrow keys to switch state
 document.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowRight') {
-    state.value = state.value === 'pending' ? 'name' : 'pending'
+    const stateTransitions = {
+      pending: 'name',
+      name: 'game',
+    }
+
+    if (stateTransitions[state.value]) {
+      state.value = stateTransitions[state.value]
+    }
   } else if (event.key === 'ArrowLeft') {
-    state.value = state.value === 'name' ? 'pending' : 'name'
+    const stateTransitions = {
+      game: 'name',
+      name: 'pending',
+    }
+
+    if (stateTransitions[state.value]) {
+      state.value = stateTransitions[state.value]
+    }
   }
 })
 </script>
