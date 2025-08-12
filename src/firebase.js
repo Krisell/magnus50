ca1c0fb76cb72303c647989f69ed1e2cad226435
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-08-12 21:00:13 +0200

    Remove revoked firebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-// The firebaseConfig below is no longer valid. Please bring your own Firebase project.
 const firebaseConfig = {
-    apiKey: 'AIzaSyA0a3N7Q_JDx4znHi-6eQfsW3r9BSwdrLQ',
-    authDomain: 'magnuskrisell50.firebaseapp.com',
-    projectId: 'magnuskrisell50',
-    storageBucket: 'magnuskrisell50.firebasestorage.app',
-    messagingSenderId: '379450567076',
-    appId: '1:379450567076:web:01c46409ec28c7ba2db931',
-    measurementId: 'G-9X4NH5Z4W8',
+
 }
 
 export const app = initializeApp(firebaseConfig)
